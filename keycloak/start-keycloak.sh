b367e55af0f385038fd3996d7112eb15a35f8e4f
Pratik94229 <pratik.941@gmail.com>
2026-09-05 22:20:06 +0530

    Use optimized Keycloak startup

--- keycloak/start-keycloak.sh
+++ keycloak/start-keycloak.sh
@@ -7,6 +7,7 @@ mkdir -p /opt/keycloak/data/import
 cp /etc/secrets/lms-realm.json /opt/keycloak/data/import/lms-realm.json
 
 exec /opt/keycloak/bin/kc.sh start \
+  --optimized \
   --http-enabled=true \
   --http-port="${PORT:-10000}" \
   --hostname="${KEYCLOAK_HOSTNAME}" \
