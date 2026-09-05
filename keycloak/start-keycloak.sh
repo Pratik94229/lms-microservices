0401ab35fae22e4941ff1d763a979c7942361e75
Pratik94229 <pratik.941@gmail.com>
2026-09-05 21:36:07 +0530

    Add Docker configuration for Keycloak

---
+++ keycloak/start-keycloak.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /opt/keycloak/data/import
+
+cp /etc/secrets/lms-realm.json /opt/keycloak/data/import/lms-realm.json
+
+exec /opt/keycloak/bin/kc.sh start \
+  --http-enabled=true \
+  --http-port="${PORT:-10000}" \
+  --hostname="${KEYCLOAK_HOSTNAME}" \
+  --proxy-headers=xforwarded \
+  --import-realm
