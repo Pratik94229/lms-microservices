4c98e81297c67f92a9793bdad6cc395362bb41f7
Pratik94229 <pratik.941@gmail.com>
2026-09-05 22:24:30 +0530

    Limit Keycloak JVM memory for Render Free

--- keycloak/start-keycloak.sh
+++ keycloak/start-keycloak.sh
@@ -6,8 +6,9 @@ mkdir -p /opt/keycloak/data/import
 
 cp /etc/secrets/lms-realm.json /opt/keycloak/data/import/lms-realm.json
 
+export JAVA_OPTS_APPEND="-Xms128m -Xmx350m"
+
 exec /opt/keycloak/bin/kc.sh start \
-  --optimized \
   --http-enabled=true \
   --http-port="${PORT:-10000}" \
   --hostname="${KEYCLOAK_HOSTNAME}" \
